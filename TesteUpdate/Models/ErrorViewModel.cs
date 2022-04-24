1c638956b49d612f21287f80521c2787ea09395a
Rafael Santos <rafael.souza108@fatec.sp.gov.br>
2022-04-24 17:08:18 -0300

    Projected created

---
+++ TesteUpdate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TesteUpdate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
